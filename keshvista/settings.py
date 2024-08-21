8e1dcc2c4b3518d099fe7bd204e70046a3bdfe26
abdurahmon-flutter <abdurahmonmahmudov222@gmail.com>
2024-08-22 01:58:04 +0300

    first commit

--- keshvista/settings.py
+++ keshvista/settings.py
@@ -133,4 +133,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_HEADERS = "access-control-allow-origin"
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-requested-with',
+    'accept',
+    'origin',
+    'x-csrftoken',
+]
